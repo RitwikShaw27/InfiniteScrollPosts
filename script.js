b09c5bafa4439850d639bda63f2fce0736339289
Ritwik <shawritwik2@gmail.com>
2022-03-03 21:02:39 +0530

    Filter fetched posts

--- script.js
+++ script.js
@@ -53,8 +53,8 @@ function filterPosts(e) {
   const posts = document.querySelectorAll(".post");
 
   posts.forEach((post) => {
-    const title = post.querySelector(".post-title").innerText.toUpperCase;
-    const body = post.querySelector(".post-body").innerText.toUpperCase;
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
 
     if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
       post.style.display = "flex";
